5025dd5306cc3f733fea8723fd5aa2b59bc9aec6
Steven <stevenortega986@gmail.com>
2025-08-13 14:09:50 -0600

    Conexion a base de datos
    Se modifica appsettings y el HomeController

--- GestionBecas/Controllers/HomeController.cs
+++ GestionBecas/Controllers/HomeController.cs
@@ -1,48 +1,62 @@
 using System.Diagnostics;
 using GestionBecas.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient; 
+using Microsoft.Extensions.Configuration;
 
 namespace GestionBecas.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly string _connectionString;
 
-        public HomeController(ILogger<HomeController> logger)
+        // Inyectamos IConfiguration para obtener la cadena de conexión
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _connectionString = configuration.GetConnectionString("ProyectoRelampagoDB");
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-        public IActionResult Becas()
-        {
-            return View("~/Views/pages/becas.cshtml");
-        }
 
-        public IActionResult Contacto()
-        {
-            return View("~/Views/pages/contacto.cshtml");
-        }
-        public IActionResult Documentos()
-        {
-            return View("~/Views/pages/documentos.cshtml");
-        }
-        public IActionResult Fomulario()
-        {
-            return View("~/Views/pages/formulario.cshtml");
-        }
+
+
+
+        public IActionResult Index() => View();
+        public IActionResult Privacy() => View();
+        public IActionResult Becas() => View("~/Views/pages/becas.cshtml");
+        public IActionResult Contacto() => View("~/Views/pages/contacto.cshtml");
+        public IActionResult Documentos() => View("~/Views/pages/documentos.cshtml");
+        public IActionResult Fomulario() => View("~/Views/pages/formulario.cshtml");
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
+
+
+
+
+
+
+
+
+        // Acción para probar la conexión
+        public IActionResult TestConexion()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(_connectionString))
+                {
+                    conn.Open();
+                }
+
+                return Content("Conexión exitosa a la base de datos");
+            }
+            catch (Exception ex)
+            {
+                return Content($"Error en la conexión: {ex.Message}");
+            }
         }
     }
 }
--- GestionBecas/obj/Debug/net8.0/GestionBecas.AssemblyInfo.cs
+++ GestionBecas/obj/Debug/net8.0/GestionBecas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionBecas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d181a58f7310428ab715dff36874c8fab771643f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ba129a779d5d3b6c1565aac25f8a77a55dfc239")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionBecas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionBecas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
